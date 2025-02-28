82f3433300ff7f36bdb95dc63f03661009f87dec
andrei.khon <andrei.khon@proton.me>
2025-02-28 14:29:39 +0100

    Lexical errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
+    let mut exit_code = 0;
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
@@ -20,6 +22,7 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
+                let mut line_number = 1;
                 let file_content_chars = file_contents.chars();
                 let _ = file_content_chars.for_each(|char| match char {
                     '(' => println!("LEFT_PAREN ( null"),
@@ -32,9 +35,16 @@ fn main() {
                     '+' => println!("PLUS + null"),
                     ';' => println!("SEMICOLON ; null"),
                     '*' => println!("STAR * null"),
-                    _ => {}
+                    '\n' => line_number += 1,
+                    _ => {
+                        eprintln!("[line {}] Error: Unexpected character: {}", line_number, char);
+                        exit_code = 65;
+                    }
                 });
                 println!("EOF  null");
+                if exit_code != 0 {
+                    exit(exit_code)
+                }
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
